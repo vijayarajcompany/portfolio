8c5fc77ef70d3e9d715f0fe983bbe38d0553b97a
vijayaraj <vijayarajsuyambu@gmail.com>
2023-01-04 12:59:26 +0400

    Add GitHub pages configuration in package.json

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM, {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const rootElement = document.getElementById('root');
+
+// 👇️ non-null (!) assertion
+const root = createRoot(rootElement!);
+
 root.render(
   <React.StrictMode>
     <App />
